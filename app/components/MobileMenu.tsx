7060cc5df1fd8c49ac3b7251bae3cab782a81128
Bini-Mo <biniyammoges1@hotmail.com>
2025-09-04 16:29:33 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is behind 'origin/main' by 3 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   app/components/MobileMenu.tsx

--- app/components/MobileMenu.tsx
+++ app/components/MobileMenu.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import Link from "next/link";
@@ -6,8 +5,14 @@ import { useState } from "react";
 
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsOpen(false); // Close the menu when a link is clicked
+  };
+
   return (
     <div className="md:hidden">
+      {/* Burger Icon */}
       <div
         className="flex flex-col gap-[4.5px] cursor-pointer"
         onClick={() => setIsOpen((prev) => !prev)}
@@ -28,13 +33,25 @@ const MobileMenu = () => {
           } origin-left ease-in-out duration-500`}
         />
       </div>
+
+      {/* Mobile Menu */}
       {isOpen && (
         <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
-          <Link href="/">Home</Link>
-          <Link href="/">Friends</Link>
-          <Link href="/">Groups</Link>
-          <Link href="/">Stories</Link>
-          <Link href="/">Login</Link>
+          <Link href="/" onClick={handleLinkClick}>
+            Home
+          </Link>
+          <Link href="/friends" onClick={handleLinkClick}>
+            Friends
+          </Link>
+          <Link href="/groups" onClick={handleLinkClick}>
+            Groups
+          </Link>
+          <Link href="/stories" onClick={handleLinkClick}>
+            Stories
+          </Link>
+          <Link href="/login" onClick={handleLinkClick}>
+            Login
+          </Link>
         </div>
       )}
     </div>
